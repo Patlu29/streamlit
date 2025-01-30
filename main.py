c6ce76e7d10810119ea853bd86f936ad28390ad4
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2025-01-30 06:06:45 +0530

    file upload method added

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import streamlit as st
 
 st.title("Simple Data(csv) Analysis")
 
+file = st.file_uploader("Choose your csv file", type=["csv", 'tsv'])
+
+if file is not None:
+    st.write("File uploaded successfully...🎉")

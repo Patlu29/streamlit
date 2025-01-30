290483b06019dae03ec0a2ed8b51942242e9cbbf
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2025-01-30 05:49:10 +0530

    data added

--- stream.py
+++ stream.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.title("My first app")
 
-st.write("Here's our first attempt at using data to create a table:")
+st.write("# Here's our first attempt at using data to create a table:")
+
+st.write("# dictionary:",{"key":["value1", "value2"], "key2":["value3", "value4"]})
